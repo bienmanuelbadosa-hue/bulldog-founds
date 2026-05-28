c693fe5b1c1294bc8e762880491524cf7159ad09
Lorcey Acosta <acostajl@students.nu-laguna.edu.ph>
2026-05-28 09:08:02 +0000

    Pending changes exported from your codespace

---
+++ backend/bulldogfounds/src/main/java/com/bulldogfounds/BulldogfoundsApplication.java
@@ -0,0 +1,13 @@
+package com.bulldogfounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulldogfoundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulldogfoundsApplication.class, args);
+	}
+
+}
